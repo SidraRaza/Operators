3520600eb49004bf9cf13206f6f1986754d66184
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2024-03-08 21:02:16 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,64 @@
+let firstName:string="Sidra"
+let secondName:string="Raza"
+let define:string="is year old"
+let age:number=17;
+let result=`${firstName+ " " +secondName} is ${age} years old`
+console.log(result)
+
+let num1:number=5;
+ let num2:number=4;
+ let total=num1+num2;
+ console.log(total);
+
+ let num3:number=5;
+ let num4:number=4;
+ let num13=num3-num4;
+ console.log(num13);
+
+
+
+let num5:number=5;
+let num6:number=4;
+let num14=num5/num6;
+console.log(num14);
+
+
+
+
+let num7:number=5;
+let num8:number=4;
+let num15=num7*num8;
+console.log(num15);
+
+
+let num9:number=5;
+let num10:number=4;
+let num16=num9**num10;
+console.log(num16);
+
+
+
+
+
+let num11:number=5;
+let num12:number=4;
+let num17=num11%num12;
+console.log(num17);
+
+
+
+
+let year=2024;
+let leap=4;
+let check=year%leap;
+console.log(check);
+
+
+
+let a:number=5;
+
+let b:number=2;
+let c:number;
+c=++a + a++ +--b +b-- +a+ b++ +b;
+console.log(c);
+6+7+1+0+5+1+2
